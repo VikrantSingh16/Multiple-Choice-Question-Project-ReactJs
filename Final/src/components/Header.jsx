f72cad357cb8f4e8cf1256705fe6f19ab74e62d0
Vikrant Singh <66239767+VikrantSingh16@users.noreply.github.com>
2021-07-23 13:03:56 +0530

    Add files via upload

---
+++ Final/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+function Header(){
+    return (
+        
+        <div class="header">
+        <h1>KBWD</h1>
+       <h3>Kon Banega Web Developer :)</h3>
+       </div>
+    
+       
+    );
+}
+export default Header;
